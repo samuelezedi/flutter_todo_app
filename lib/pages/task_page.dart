debc0f1950a468ac391722d2c60f7a46484750a5
Samuel Ezedi <samuelezedi@gmail.com>
2020-06-06 09:18:00 +0100

    new changes and upgrades

---
+++ lib/pages/task_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TaskPage extends StatefulWidget {
+  @override
+  _TaskPageState createState() => _TaskPageState();
+}
+
+class _TaskPageState extends State<TaskPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
