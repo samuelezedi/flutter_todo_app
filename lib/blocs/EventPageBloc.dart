2e30d802650c9ec7f3bf88eea4b2a91d6eb0f85d
Samuel Ezedi <samuelezedi@gmail.com>
2020-06-06 10:37:19 +0100

    new changes and upgrades

---
+++ lib/blocs/EventPageBloc.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Event {
+  final String time;
+  final String task;
+  final String desc;
+  final bool isFinish;
+
+  Event(this.time, this.task, this.desc, this.isFinish);
+
+}
