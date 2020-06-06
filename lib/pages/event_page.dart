3c68aed1a76c15daf5b23a36328e79a47aa3f481
Samuel Ezedi <samuelezedi@gmail.com>
2020-06-06 09:43:19 +0100

    new changes and upgrades

--- lib/pages/event_page.dart
+++ lib/pages/event_page.dart
@@ -16,25 +16,36 @@ class _EventPageState extends State<EventPage> {
             Icon(Icons.fiber_manual_record, size: 20,
                     color: Theme.of(context).accentColor,
             ),
-            Text('08.00'),
             Container(
-              padding: EdgeInsets.all(14.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(Radius.circular(12)),
-                boxShadow: [BoxShadow(
-                  color: Color(0x20000000),
-                  blurRadius: 5,
-                  offset: Offset(0,3)
-                )]
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text('Coffee with Sam'),
-                  SizedBox(height: 12,),
-                  Text('Personal')
-                ],
+              width: 80,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Text('08.00'),
+                )
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 12.0, bottom: 12.0),
+                child: Container(
+                  padding: EdgeInsets.all(14.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(12)),
+                    boxShadow: [BoxShadow(
+                      color: Color(0x20000000),
+                      blurRadius: 5,
+                      offset: Offset(0,3)
+                    )]
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Coffee with Sam'),
+                      SizedBox(height: 12,),
+                      Text('Personal')
+                    ],
+                  ),
+                ),
               ),
             )
           ],
